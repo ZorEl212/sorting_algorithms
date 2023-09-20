619c99ca24f699879a3927e1c55f80a2d616f751
ZorEl212 <yabsirad212@gmail.com>
2023-09-20 13:59:54 -0400

    Bubble sort algorithm and it's time complexity values

---
+++ 0-bubble_sort.c
@@ -0,0 +1,43 @@
+#include "sort.h"
+
+/**
+* swap_values - swap two values
+* @num1: the first number
+* @num2: the second number
+*
+* Return: Nothing
+*/
+void swap_values(int *num1, int *num2)
+{
+	int temp = *num1;
+
+	*num1 = *num2;
+	*num2 = temp;
+}
+
+/**
+* bubble_sort - bubble sort algorithm
+* @array: array to sort
+* @size: array size
+*
+* Return: Nothing
+*/
+void bubble_sort(int *array, size_t size)
+{
+	size_t i, j, swapped = 0;
+
+	for (i = 0; i < size - 1; i++)
+	{
+		for (j = 0; j < size - i - 1; j++)
+		{
+			if (array[j] > array[j + 1])
+			{
+				swap_values(&array[j], &array[j + 1]);
+				swapped = 1;
+			}
+		}
+
+		if (!swapped)
+			break;
+	}
+}
